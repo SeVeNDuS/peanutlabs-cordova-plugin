0976cd01d4cdb3d90ee5bfeb703ae3ae7d18d2c3
SeVeNDuS <sevendus@gmail.com>
2015-11-07 16:37:57 +0100

    Added peanutlabs sdk

--- android/PeanutlabsPlugin.java
+++ android/PeanutlabsPlugin.java
@@ -19,8 +19,8 @@ import org.json.JSONObject;
 public class FyberPlugin extends CordovaPlugin {
 
     private static final String LOGTAG = "PeanutlabsPlugin";
-    private static final String DEFAULT_APP_ID = "27434";
-    private static final String DEFAULT_APP_KEY = "27434";
+    private static final String DEFAULT_APP_ID = "8201";
+    private static final String DEFAULT_APP_KEY = "4fbd9cfce53903b181bd13b1996b4d21";
 
     private static final String ACTION_INITIALIZE = "initialize";
     private static final String ACTION_SHOW_OFFERWALL = "showOfferwall";
